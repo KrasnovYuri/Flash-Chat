681ad1fb92c6c74de7aa5cf92eddaa3b58ffa411
KrasnovYuri <krasnov1yuri@gmail.com>
2023-12-21 18:42:56 +0300

    add firebase

--- Flash Chat/Controllers/ChatViewController.swift
+++ Flash Chat/Controllers/ChatViewController.swift
@@ -13,13 +13,33 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextField: UITextField!
     
+    let db = Firestore.firestore()
+    
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hey!"),
+        Message(sender: "a@b.com", body: "Hello!"),
+        Message(sender: "1@2.com", body: "What's up?gfgdhdghjbdlbndl;kf;okhj df ljf;lrkgjdf;lbv;kl jdf; jdf df;lgj  dflgjkdf;lkgjdflbv    dflkgjdlk jdflkgjdflgjdfkljb;dkjg[iordghj;dfkljbdl;igjdfkl;jbndkl;jgbodfijbdfkln")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
         title = K.appName
         navigationItem.hidesBackButton = true
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        guard let messageBody = messageTextField.text else { return }
+        guard let messageSender = Auth.auth().currentUser?.email else { return }
+        db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender, K.FStore.bodyField: messageBody]) { error in
+            if let error {
+                print(error)
+            } else {
+                print("Succes")
+            }
+        }
     }
     
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
@@ -31,6 +51,18 @@ class ChatViewController: UIViewController {
           print("Error signing out: %@", signOutError)
         }
     }
-    
-
 }
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        
+        return cell
+    }
+}
+
